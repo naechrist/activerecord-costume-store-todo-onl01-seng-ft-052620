44b52c736c16c2f75d0a75b5c2624663c929dca3
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-28 17:31:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "costumes", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.string "size"
-    t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "custome_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
